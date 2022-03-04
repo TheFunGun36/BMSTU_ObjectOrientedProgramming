dddc5dcf0bc3d7a7a0c9107b849b30ef2cc09a72
Cheprasov Kirill <maki-333@yandex.ru>
2022-03-04 13:56:48 +0300

    Optimized rotation function

--- lab1/Model3D.cpp
+++ lab1/Model3D.cpp
@@ -1,32 +1,52 @@
 #include "Model3D.h"
 
 namespace wireframe {
-    QPointF rotate(QPointF point, QPointF center, qreal angle) {
-        qreal sint = qSin(angle);
-        qreal cost = qCos(angle);
+    struct AngleInfo {
+        qreal sint;
+        qreal cost;
+    };
 
+    QPointF rotate(QPointF point, QPointF center, const AngleInfo &a) {
         qreal dx = point.x() - center.x();
         qreal dy = point.y() - center.y();
 
-        point.setX(center.x() + dx * cost + dy * sint);
-        point.setY(center.y() - dx * sint + dy * cost);
+        point.setX(center.x() + dx * a.cost + dy * a.sint);
+        point.setY(center.y() - dx * a.sint + dy * a.cost);
 
         return point;
     }
 
     void modelRotate(Model3D &model, QVector3D point, QVector3D angles) {
+        if (!qFuzzyIsNull(angles.x())) {
+            AngleInfo ax;
+            ax.sint = qSin(angles.x());
+            ax.cost = qCos(angles.x());
+
+            for (Vertex3D &v : model.vertex) {
+                QPointF r = rotate({ v.y, v.z }, { point.y(), point.z() }, ax);
+                v.y = r.x();
+                v.z = r.y();
+            }
+        }
+
+        if (!qFuzzyIsNull(angles.y())) {
+            AngleInfo ay;
+            ay.sint = qSin(angles.y());
+            ay.cost = qCos(angles.y());
+
+            for (Vertex3D &v : model.vertex) {
+                QPointF r = rotate({ v.x, v.z }, { point.x(), point.z() }, ay);
+                v.x = r.x();
+                v.z = r.y();
+            }
+        }
+
         for (Vertex3D &v : model.vertex) {
-            QPointF r;
+            AngleInfo az;
+            az.sint = qSin(angles.z());
+            az.cost = qCos(angles.z());
 
-            r = rotate({ v.y, v.z }, { point.y(), point.z() }, angles.x());
-            v.y = r.x();
-            v.z = r.y();
-
-            r = rotate({ v.x, v.z }, { point.x(), point.z() }, angles.y());
-            v.x = r.x();
-            v.z = r.y();
-
-            r = rotate({ v.x, v.y }, { point.x(), point.y() }, angles.z());
+            QPointF r = rotate({ v.x, v.y }, { point.x(), point.y() }, az);
             v.x = r.x();
             v.y = r.y();
         }
