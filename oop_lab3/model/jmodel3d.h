ade72b95f2155d49c119271032b351038a267950
jora <maki-333@yandex.ru>
2022-06-09 12:51:49 +0300

    everything is broken. Im tired

--- oop_lab3/model/jmodel3d.h
+++ oop_lab3/model/jmodel3d.h
@@ -11,26 +11,17 @@ class Model3D : public SceneObject {
 public:
     using ImplPtr = std::shared_ptr<Model3DImpl>;
 
-    Model3D(const ImplPtr& implementation, const std::string& label);
+    Model3D(const ImplPtr& implementation, const std::string& label = "Model");
+    Model3D(const std::string& label = "Model");
     Model3D(const Model3D& other) = delete;
     Model3D(Model3D&& other) noexcept;
     virtual ~Model3D() = default;
 
-    virtual Vector3D& point(size_t index);
-    virtual Vector3D& operator[](size_t index);
-    virtual const Vector3D& point(size_t index) const;
-    virtual const Vector3D& operator[](size_t index) const;
-    virtual size_t pointsAmount() const noexcept;
-
-    virtual bool addPoint(const Vector3D& point) noexcept;
-    virtual Model3D& operator<<(const Vector3D& point);
-
     virtual operator bool() const noexcept;
-    virtual bool isEmpty() const noexcept;
     virtual bool isValid() const noexcept;
 
-    inline virtual ImplPtr implementation() noexcept { return _implementation; }
-    inline virtual const Model3DImpl& implementation() const noexcept { return *_implementation; }
+    virtual ImplPtr implementation() noexcept;
+    virtual void setImplementation(const ImplPtr& implementation) noexcept;
 
 protected:
     ImplPtr _implementation;
@@ -38,8 +29,6 @@ protected:
 private:
     virtual std::ostream& addToStream(std::ostream& stream) const override;
     virtual std::wostream& addToStream(std::wostream& stream) const override;
-
-    std::vector<Vector3D> _points;
 };
 
 }
---
+++ oop_lab3/model/jsceneobject.cpp
@@ -0,0 +1,109 @@
+#include "jsceneobject.h"
+
+namespace Jora {
+
+SceneObject::SceneObject(const std::string& _label)
+    : Visible(this, &Self::visible, &Self::setVisible)
+    , Label(this, &Self::label, &Self::setLabel)
+    , Type(this, &Self::type)
+    , Id(this, &Self::id)
+    , _visible(false)
+    , _label(_label)
+    , _transform() {
+    _id = generateId();
+}
+
+const bool& SceneObject::visible() const noexcept {
+    return _visible;
+}
+
+const std::string& SceneObject::label() const noexcept {
+    return _label;
+}
+
+const std::type_info& SceneObject::type() const noexcept {
+    return typeid(*this);
+}
+
+const size_t& SceneObject::id() const noexcept {
+    return _id;
+}
+
+const Transform& SceneObject::transform() const noexcept {
+    return _transform;
+}
+
+Transform& SceneObject::transform() noexcept {
+    return _transform;
+}
+
+void setVisible(const bool& _visible) noexcept {
+    // beeba! Yaaaay
+}
+
+void SceneObject::setLabel(const std::string& label) noexcept {
+    _label = label;
+}
+
+SceneObject::SelfRef SceneObject::operator[](size_t id) noexcept {
+    return *this;
+}
+
+SceneObject::SelfCRef SceneObject::operator[](size_t id) const noexcept {
+    return *this;
+}
+
+bool SceneObject::isComposite() const {
+    return false;
+}
+
+bool SceneObject::insert(const SceneObject::SelfPtr& sceneObject) {
+    return false;
+}
+
+bool SceneObject::remove(size_t id) noexcept {
+    return false;
+}
+
+bool SceneObject::remove(SceneObject::IteratorConst it) noexcept {
+    return false;
+}
+
+bool SceneObject::contains(size_t id) const noexcept {
+
+}
+
+size_t SceneObject::count() const noexcept {
+    return 1;
+}
+
+SceneObject::Iterator SceneObject::begin() noexcept {
+    return SceneObject::Iterator();
+}
+
+SceneObject::Iterator SceneObject::end() noexcept {
+    return SceneObject::Iterator();
+}
+
+SceneObject::IteratorConst SceneObject::begin() const noexcept {
+    return SceneObject::IteratorConst();
+}
+
+SceneObject::IteratorConst SceneObject::end() const noexcept {
+    return SceneObject::IteratorConst();
+}
+
+SceneObject::IteratorConst SceneObject::cbegin() const noexcept {
+    return SceneObject::IteratorConst();
+}
+
+SceneObject::IteratorConst SceneObject::cend() const noexcept {
+    return SceneObject::IteratorConst();
+}
+
+size_t SceneObject::generateId() {
+    static size_t id = 0;
+    return id++;
+}
+
+}
